110d81399d259c11fcdc57c60694280b3e23eb3e
Nathan Alden <visualcsharpcoder@hotmail.com>
2011-11-27 15:38:41 -0600

    Added check for null

--- Source/Data/ConfigurationManagerConnectionStringProvider.cs
+++ Source/Data/ConfigurationManagerConnectionStringProvider.cs
@@ -1,4 +1,7 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
+
+using Junior.Common;
 
 namespace Junior.Persist.Data
 {
@@ -12,8 +15,11 @@ namespace Junior.Persist.Data
 		/// </summary>
 		/// <param name="key">A key.</param>
 		/// <returns>The connection string for the specified key.</returns>
+		/// <exception cref="ArgumentNullException">Thrown when <paramref name="key"/> is null.</exception>
 		public string ByKey(string key)
 		{
+			key.ThrowIfNull("key");
+
 			return ConfigurationManager.ConnectionStrings[key].ConnectionString;
 		}
 	}
